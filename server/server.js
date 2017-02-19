0658b66849e021ec51efe2b009aa0dc1e43adf1d
Taylor <tbaustin@eagles.usi.edu>
2017-02-18 20:32:36 -0600

    updated 2/18/17

--- server/server.js
+++ server/server.js
@@ -5,34 +5,50 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 var Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 500,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
   }
 });
 
-// var newTodo = new Todo({
-//   text: 'Cook dinner'
+// var secondTodo = new Todo({
+//   text: 'Something to do'
 // });
 //
-// newTodo.save().then((doc) => {
-//   console.log('Saved todo', doc);
+// secondTodo.save().then((doc) => {
+//   console.log('Saved 2nd todo', JSON.stringify(doc, undefined, 2));
 // }, (e) => {
-//   console.log('Unable to save todo', e);
+//   console.log(e);
 // });
 
-var secondTodo = new Todo({
-  text: 'Go to school',
-  completed: false,
-  completedAt: 123
+// User model
+// email - require it - trim it - set type - set min length of 1
+
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
+  }
 });
 
-secondTodo.save().then((doc)=> {
-  console.log('Saved 2nd todo', JSON.stringify(doc, undefined, 2));
+var user1 = new User({
+  email: 'tbaustin1992@gmail.com'
+});
+
+user1.save().then((doc) => {
+  console.log('Saved User', JSON.stringify(doc, undefined, 2));
 }, (e) => {
-  console.log(e);
-})
+  console.log('Something went wrong', (e));
+});
