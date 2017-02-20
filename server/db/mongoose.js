562055dc87bf15659da5a5c9d070a64e6d4b7f1a
Taylor <tbaustin@eagles.usi.edu>
2017-02-20 09:38:48 -0600

    Setup seperate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,11 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-let db = {
-  localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: 'mongodb://tbaustin:number1twin@ds157529.mlab.com:57529/tbaustin-node-todo-api'
-}
-
-mongoose.connect(process.env.PORT ? db.mlab : db.localhost);
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
