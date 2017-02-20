729e21be816c4eef2c8b7a2b0e74e20428d8cc20
Taylor <tbaustin@eagles.usi.edu>
2017-02-20 08:35:16 -0600

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
+//**************Local Requires*****************
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
 var {User} = require('./models/user.js');
 
+//------------------------------------------
 var app = express();
 const port = process.env.PORT || 3000;
 
@@ -70,6 +73,37 @@ app.delete('/todos/:id', (req, res) => {
   });
 });
 
+//*****************UPDATE***************
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  //Using lodash _.pick lets us choose what the user can update.
+  //here we only let the user update the text and completed properties.
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if(_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if(!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+//***********Connection to server and export**************
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
 });
