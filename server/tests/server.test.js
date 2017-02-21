21ecf623c3a55b51c416366f3884ab3152a23c52
Taylor <tbaustin@eagles.usi.edu>
2017-02-21 11:39:02 -0600

    Add test to DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -275,6 +275,25 @@ describe('DELETE /todos/:id', () => {
 
 });
 
+describe('DELETE /users/me/token', () => {
+  it('should remove auth token on logout', (done) => {
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .end((err, res) => {
+        if(err) {
+          return done();
+        }
+
+        User.findById(users[0]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+});
+
 //**************TEST UPDATE**********************
 describe('PATCH /todos/:id', () => {
   it('should update the todo', (done) => {
