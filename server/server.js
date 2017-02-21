412a8f5cfadfcc1d21ad16adc4587dd0a9a90262
Taylor <tbaustin@eagles.usi.edu>
2017-02-21 08:00:05 -0600

    Add GET /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
 var {User} = require('./models/user.js');
+var {authenticate} = require('./middleware/authenticate.js');
 
 //------------------------------------------
 var app = express();
@@ -67,6 +68,10 @@ app.get('/todos/:id', (req, res) => {
   }).catch((e) => {
     res.status(400).send();
   });
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 //************DELETE*******************
