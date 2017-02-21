5cb65839df50cddb03bee6126d70f7e0d7af1363
Taylor <tbaustin@eagles.usi.edu>
2017-02-21 10:18:05 -0600

    Add POST users/login route

--- server/server.js
+++ server/server.js
@@ -43,6 +43,18 @@ app.post('/users', (req, res) => {
   });
 });
 
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
 //*****************GET********************
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
