3c8d17f123572d383a00acb174a13af89bad81d9
Taylor <tbaustin@eagles.usi.edu>
2017-02-22 11:38:31 -0600

    change config file

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,7 @@
 var env = process.env.NODE_ENV || 'development';
 console.log('env ******', env);
 
-if (env === 'development' || env === 'test' || env === 'production') {
+if (env === 'development' || env === 'test') {
   var config = require('./config.json');
   var envConfig = config[env];
 
